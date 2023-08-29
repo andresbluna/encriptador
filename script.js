8fc364564b231d617c2d98a80aa998a8ddf57b5e
andresbluna <andres.benjamin.luna@gmail.com>
2023-08-28 22:53:10 -0400

    Tweaked the encryption app a bit for web & mobile

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+$(document).ready(function() {
+    // Funcion para encriptar el texto
+    $("#encryptBtn").click(function() {
+        var plaintext = $(".textarea").val();
+        var encryptedText = CryptoJS.AES.encrypt(plaintext, "secret key");
+        $("#output").text(encryptedText);
+    });
 
+    // Funcion para desencriptar el texto
+    $("#decryptBtn").click(function() {
+        var encryptedText = $(".textarea").val();
+        var bytes = CryptoJS.AES.decrypt(encryptedText, "secret key");
+        var originalText = bytes.toString(CryptoJS.enc.Utf8);
+        $("#output").text(originalText);
+    });
+
+    // Funcion para copiar el texto
+    $("#copyBtn").click(function() {
+        var temp = $("<textarea>");
+        $("body").append(temp);
+        temp.val($("#output").text()).select();
+        document.execCommand("copy");
+        temp.remove();
+    });
+});
